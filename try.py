8612d754fca0bb7f2321dea778802505fdca7086
jt <jt.ascend@gmail.com>
2016-09-13 16:47:01 +1000

    Download local and Upload to different bucket

--- try.py
+++ try.py
@@ -1,12 +1,17 @@
 import boto3
 import os
 objectKey = 'hworld.txt'
-bucketNameInput = 'beachbucket'
+sourceBucketName = 'SourceBucket'
+destBucketName = 'DestBucket'
 localFilename = '/tmp/{}'.format(os.path.basename(objectKey))
-
 s3 = boto3.resource('s3')
-inputBucket = s3.Bucket(bucketNameInput)
-inputBucket.download_file(objectKey,localFilename)
-print localFilename
-print(open(localFilename).read())
+sourceBucket = s3.Bucket(sourceBucketName)
+sourceBucket.download_file(objectKey,localFilename)
+#do some local processing...
+destBucket = s3.Bucket(destBucketName)
+destObjectKey = destBucket.Object(objectKey)
+with open(localFilename,'rb') as data:
+    destObjectKey.upload_fileobj(data)
+    print 'Upload to to dest bucket'
+
 print 'EOF'
