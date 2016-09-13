ccb070a5157062e890f26ec3233e011df8da6eaa
jt <jt.ascend@gmail.com>
2016-09-13 16:28:57 +1000

    Updated to download using boto resource

--- try.py
+++ try.py
@@ -3,8 +3,10 @@ import os
 objectKey = 'hworld.txt'
 bucketNameInput = 'beachbucket'
 localFilename = '/tmp/{}'.format(os.path.basename(objectKey))
-s3 = boto3.client('s3')
-s3.download_file(bucketNameInput,objectKey,localFilename)
+
+s3 = boto3.resource('s3')
+inputBucket = s3.Bucket(bucketNameInput)
+inputBucket.download_file(objectKey,localFilename)
 print localFilename
 print(open(localFilename).read())
 print 'EOF'
